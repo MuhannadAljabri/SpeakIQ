047e7a6b30f5c52876e8d3368668eb261629df71
MuhannadAljabri <97546261+MuhannadAljabri@users.noreply.github.com>
2023-11-11 11:24:35 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import '../Screens/splash.dart';
 import 'Screens/Onboarding.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
