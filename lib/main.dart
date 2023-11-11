d614b78a759f1faa949af49e4d64968176629dea
MuhannadAljabri <97546261+MuhannadAljabri@users.noreply.github.com>
2023-11-11 12:33:38 -0500

    Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import '../Screens/splash.dart';
 import 'Screens/Onboarding.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
