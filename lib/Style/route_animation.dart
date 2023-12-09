ec7b096195b2cf871e6f865b22b6752f5d6a3d37
gslau02 <114629753+gslau02@users.noreply.github.com>
2023-12-09 08:20:27 +0800

    Add slidingFromRight

--- lib/Style/route_animation.dart
+++ lib/Style/route_animation.dart
@@ -19,6 +19,24 @@ Route slidingFromLeft(pageName) {
   );
 }
 
+PageRouteBuilder slidingFromRight(pageName) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => pageName,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(-1.0, 0.0); // Change the begin offset
+      const end = Offset.zero;
+      const curve = Curves.easeInOut;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
 Route slidingFromDown(pageName) {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => pageName,
