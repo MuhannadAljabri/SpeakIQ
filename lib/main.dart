ae83542523e183e3ed4cc9d0f200228ab0efda0a
gslau02 <114629753+gslau02@users.noreply.github.com>
2024-01-26 02:24:09 -0500

    Change the home route to home navigation screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:speak_iq/Screens/forgot_password.dart';
 import '../Screens/splash.dart';
 import '../Screens/UserSignup.dart';
 import '../Screens/login.dart';
-import '../Screens/Home.dart';
 import '../Screens/SpeakerProfile.dart';
 import '../Screens/Onboarding.dart';
+import '../Screens/home_navigation_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -25,32 +25,15 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       // Routes
       routes: {
-        '/': (context) => SplashScreen(),
+        '/': (context) => const SplashScreen(),
         '/user_signup': (context) => const UserSignup(),
-        '/home': (context) => HomePage(),
+        '/home': (context) => HomeNavigationScreen(),
         '/onboarding': (context) => OnboardingScreen(),
         '/login': (context) => const LoginScreen(),
-        '/speaker_signup': (context) => SpeakerSignUp(),
+        '/speaker_signup': (context) => const SpeakerSignUp(),
         '/forgot_password': (context) => const ForgotPasswordScreen(),
-        '/speaker_profile': (context) => SpeakerProfile()
+        '/speaker_profile': (context) => const SpeakerProfile()
       },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  // Placeholder homescreen
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main Screen'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the main screen!'),
-      ),
     );
   }
 }
