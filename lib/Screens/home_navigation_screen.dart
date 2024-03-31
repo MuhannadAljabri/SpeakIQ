eaf7aa151db5bda27fe3e055464faa22e5d8f564
gslau02 <114629753+gslau02@users.noreply.github.com>
2024-03-31 02:18:26 -0400

    Added Safe Area

--- lib/Screens/home_navigation_screen.dart
+++ lib/Screens/home_navigation_screen.dart
@@ -24,63 +24,64 @@ class HomeNavigationScreenState extends State<HomeNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     double screenHeight = Platform.isIOS? 92.0 : 85.0;
-    return Scaffold(
-      body: _pages[_currentIndex],
-      
-      bottomNavigationBar: Container(
-          height: screenHeight,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12.0),
-              topRight: Radius.circular(12.0),
+    return SafeArea(child: 
+      Scaffold(
+        body: _pages[_currentIndex],
+        
+        bottomNavigationBar: Container(
+            height: screenHeight,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12.0),
+                topRight: Radius.circular(12.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(255, 223, 223, 223),
+                  blurRadius: 5.0,
+                  spreadRadius: 0.5,
+                  offset: Offset(0, -4.0),
+                ),
+              ],
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(255, 223, 223, 223),
-                blurRadius: 5.0,
-                spreadRadius: 0.5,
-                offset: Offset(0, -4.0),
-              ),
-            ],
-          ),
-          child: BottomNavigationBar(
-            backgroundColor: Colors.transparent,
-            selectedItemColor: ColorsReference.darkBlue,
-            unselectedItemColor: Colors.grey,
-            currentIndex: _currentIndex,
-            elevation:0.0, 
-            items: [
-              BottomNavigationBarItem(
-                icon: SvgPicture.asset(
-                  'assets/home_icon.svg',
-                  width: 24,
-                  height: 24,
-                  color: _currentIndex == 0
-                      ? ColorsReference.darkBlue
-                      : Colors.grey,
+            child: BottomNavigationBar(
+              backgroundColor: Colors.transparent,
+              selectedItemColor: ColorsReference.darkBlue,
+              unselectedItemColor: Colors.grey,
+              currentIndex: _currentIndex,
+              elevation:0.0, 
+              items: [
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    'assets/home_icon.svg',
+                    width: 24,
+                    height: 24,
+                    color: _currentIndex == 0
+                        ? ColorsReference.darkBlue
+                        : Colors.grey,
+                  ),
+                  label: 'Home',
                 ),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: SvgPicture.asset(
-                  'assets/profile_icon.svg',
-                  width: 24,
-                  height: 24,
-                  color: _currentIndex == 1
-                      ? ColorsReference.darkBlue
-                      : Colors.grey,
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    'assets/profile_icon.svg',
+                    width: 24,
+                    height: 24,
+                    color: _currentIndex == 1
+                        ? ColorsReference.darkBlue
+                        : Colors.grey,
+                  ),
+                  label: 'Profile',
                 ),
-                label: 'Profile',
-              ),
-            ],
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-          )),
-    
-    );
+              ],
+              onTap: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            )),
+      )
+  );
   }
 }
