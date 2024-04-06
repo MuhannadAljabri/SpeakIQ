b52859e97f07af133e7ac5ca2ce464b5c647a177
Gabriel Gonzales <gabgonz@umich.edu>
2024-04-06 15:05:53 -0400

    Changed Onboarding Message

--- lib/Screens/Onboarding.dart
+++ lib/Screens/Onboarding.dart
@@ -36,72 +36,74 @@ class _Onboard extends State<OnboardingScreen> {
 
         // Colored box from the bottom to the middle
         Expanded(
-        child: Container(
-          height: MediaQuery.of(context).size.height / 2,
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 245, 245, 245),
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(100),
-              topLeft: Radius.circular(100),
-            ),
-          ),
-          child: Column(
-            children: [
-              const Text(
-                'Text about the features',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    height: 5,
-                    fontWeight: FontWeight.bold),
+          child: Container(
+            height: MediaQuery.of(context).size.height / 2,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 245, 245, 245),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(100),
+                topLeft: Radius.circular(100),
               ),
-              const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
+            ),
+            child: Column(
+              children: [
+                const Text(
+                  'Welcome to Speaksy!',
+                  style: TextStyle(
                       color: Colors.black,
-                      fontSize: 18,
-                    ),
-                  )),
-              SizedBox(height: 40),
-              Row(
-                children: [
-                  Spacer(),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(slidingFromLeft(LoginScreen()));
-                    },
-                    child: const Text(
-                      'GET STARTED',
+                      fontSize: 20,
+                      height: 5,
+                      fontWeight: FontWeight.bold),
+                ),
+                const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      'Speaksy is a platform designed to help event organizers and individuals to find speakers based off languages and specialized topics. The app provides detailed profiles to view, as well as any relevant information needed when picking your desired speaker.',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        color: ColorsReference.lightBlue,
-                        fontFamily: 'Poppins',
-                        fontSize: 16,
-                        letterSpacing:
-                            0 /*percentages not used in flutter. defaulting to zero*/,
-                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    )),
+                SizedBox(height: 40),
+                Row(
+                  children: [
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context)
+                            .push(slidingFromLeft(LoginScreen()));
+                      },
+                      child: const Text(
+                        'GET STARTED',
+                        style: TextStyle(
+                          color: ColorsReference.lightBlue,
+                          fontFamily: 'Poppins',
+                          fontSize: 16,
+                          letterSpacing:
+                              0 /*percentages not used in flutter. defaulting to zero*/,
+                          fontWeight: FontWeight.normal,
+                        ),
                       ),
                     ),
-                  ),
-                  IconButton(
-                    padding: EdgeInsets.only(left: 10, right: 35),
-                    iconSize: 28,
-                    icon: const Icon(
-                      Icons.arrow_forward_ios_rounded,
-                      color: ColorsReference.lightBlue,
+                    IconButton(
+                      padding: EdgeInsets.only(left: 10, right: 35),
+                      iconSize: 28,
+                      icon: const Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: ColorsReference.lightBlue,
+                      ),
+                      onPressed: () {
+                        // Navigate back to the previous screen
+                        Navigator.of(context)
+                            .push(slidingFromLeft(LoginScreen()));
+                      },
                     ),
-                    onPressed: () {
-                      // Navigate back to the previous screen
-                      Navigator.of(context).push(slidingFromLeft(LoginScreen()));
-                    },
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
         ),
       ],
     );
